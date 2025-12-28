a2a7eb86c7dc085cdaaea8522726cdd20f542a55
Arbaz-Ahmad95 <arbazahmad363@gmail.com>
2025-12-28 23:51:42 +0530

    Created components Process Flow and Services Tree

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import ServicesSection from './Pages/ServicesSection'
+
 import WhyTrustUs from './components/WhyTrustUs'
 import Hero from "./components/Hero"
 import Footer from './components/Footer'
+import ServicesTree from './components/ServicesTree'
+import ProcessFlow from './components/ProcessFlow'
+
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
-       <ServicesSection/>
+      <ServicesTree/>
+      <ProcessFlow/>
+     
+       
       <WhyTrustUs/>
       <Footer/>
     </div>
